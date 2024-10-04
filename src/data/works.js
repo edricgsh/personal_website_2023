13f4744890cda8357409d95d624423582c2ec1e8
gun99-dev <edricgan.44@gmail.com>
2024-10-04 20:04:15 +0800

    Change work image url

--- src/data/works.js
+++ src/data/works.js
@@ -3,7 +3,7 @@ const myWorks = [
 		company: "Ant Group",
 		jobTitle: "Senior Software Engineer",
 		image: {
-			imgUrl: "./ant_group.jpg",
+			imgUrl: "/portfolio/ant_group.jpg",
 			imgAlt: "Ant Group",
 		},
 		duration: "2020 Feb - Present",
@@ -12,7 +12,7 @@ const myWorks = [
 		company: "Visa",
 		jobTitle: "Full Stack Software Engineer",
 		image: {
-			imgUrl: "./visa_logo.jpg",
+			imgUrl: "/portfolio/visa_logo.jpg",
 			imgAlt: "Visa",
 		},
 		duration: "2018 July - 2020 Feb",
